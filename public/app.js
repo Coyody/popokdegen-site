52338a7275ad23d21335ba3efcccfd871b31b0db
Coyody <codyback94@gmail.com>
2026-08-17 20:14:10 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -912,7 +912,7 @@ function showDegenUnlockToast(degen) {
 
       if (
         comboCount > 0 &&
-        comboCount % 5 === 0
+        comboCount % 50 === 0
       ) {
         showTheresMoreEvent();
       }
